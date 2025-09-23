c722d223537cee604fcd9e0238283b5ad1e6350f
lauvergn <David.Lauvergnat@universite-paris-saclay.fr>
2025-09-23 17:53:04 +0200

    dev2 with the new makefile (temp version)

--- scripts/dependency.sh
+++ scripts/dependency.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-name_dep=dependencies.mk
-SRCFile=fortranlist.mk
+name_dep=scripts/dependencies.mk
+SRCFile=scripts/fortranlist.mk
 
 list=`ls SRC/*.f90 SRC/*/*.f90`
-ExcludeList='FormicAcid_m.f90 H3-1_m.f90 PH4JoOri_m.f90 PH4Jo2_m.f90'
+ExcludeList=''
 
 echo "#===============================================" > $name_dep
 echo "#===============================================" > $SRCFile
@@ -24,7 +24,10 @@ echo "#===============================================" >> $name_dep
 for ff90 in $list
 do
    ff=`awk '{name=$1 ; n=split(name,tab,"/") ; if (n > 0) {l=length(tab[n]) ; print tab[n]}}' <<< $ff90`
+   #echo '# '$ff >> $name_dep
    if grep -vq $ff <<< $ExcludeList;  then
-     awk -f scripts/dep2.awk $ff90 >> $name_dep
+     mname=`awk -f scripts/get_modname.awk $ff90`
+     #echo "#mname: " $mname >> $name_dep
+     awk -v mod_name=$mname -f scripts/dep2.awk $ff90 >> $name_dep
    fi
 done
